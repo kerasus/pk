7c5c98b72dc75a2c7e0d16377f2dc381b9246b4f
neo <neo.kazemi168@gmail.com>
2022-01-22 10:59:23 +0330

    user create

--- pages/user/create/index.vue
+++ pages/user/create/index.vue
@@ -6,10 +6,6 @@
     <v-col md="12">
       <v-row :style="{ padding: '20px 10px' }">
         <v-col md="2" class="vertialcally-center-items">
-          تعداد کاربران ذخیره شده:
-          {{ savedNumber }} از {{ completeUserNumber }}
-        </v-col>
-        <v-col md="2" class="vertialcally-center-items">
           <v-btn color="green" dark @click="save">
             ذخیره
             <v-icon :style="{ marginRight: '10px' }">
@@ -20,110 +16,91 @@
       </v-row>
       <v-form ref="form" lazy-validation>
         <v-row v-for="user in userForm" :key="user.key">
-          <v-col md="11" class="user-row">
+          <v-col md="11" class="user-row" :class="{ 'has-been-saved': user.hasBeenSaved }">
             <div class="input-box">
-              <v-text-field
-                dense
-                v-model="user.firstName"
-                :disabled="!user.editable"
-                @input="user.hasBeenSaved = false"
-                outlined
-                label="نام"
-                :rules="[user.firstName_error]"
-              />
+              <div class="form-input">
+                <label>
+                  <input :class="{ 'has-error': user.firstName_error }" required type="text" v-model="user.firstName"  @change="user.hasBeenSaved = false">
+                  <span class="placeholder">نام</span>
+                </label>
+                <span class="error-message" v-if="user.firstName_error">{{ user.firstName_error }}</span>
+              </div>
             </div>
             <div class="input-box">
-              <v-text-field
-                dense
-                v-model="user.lastName"
-                :disabled="!user.editable"
-                @input="user.hasBeenSaved = false"
-                outlined
-                label="نام خانوادگی"
-                :rules="[user.lastName_error]"
-              />
+              <div class="form-input">
+                <label>
+                  <input :class="{ 'has-error': user.lastName_error }" required type="text" v-model="user.lastName"  @change="user.hasBeenSaved = false">
+                  <span class="placeholder">نام خانوادگی</span>
+                </label>
+                <span class="error-message" v-if="user.lastName_error">{{ user.lastName_error }}</span>
+              </div>
             </div>
             <div class="input-box">
-              <v-select
-                dense
-                v-model="user.gender_id"
-                label="جنسیت"
-                :disabled="!user.editable"
-                @input="user.hasBeenSaved = false"
-                outlined
-                :items="genders"
-                item-text="title"
-                item-value="id"
-                :rules="[user.gender_id_error]"
-              />
+              <div class="select">
+                <select class="select-text" required v-model="user.gender_id" :class="{ 'has-error': user.gender_id_error }">
+                  <option value="" disabled selected></option>
+                  <option v-for="(item, index) in genders" :key="index" :value="item.id">{{ item.title }}</option>
+                </select>
+                <span class="select-highlight"></span>
+                <span class="select-bar"></span>
+                <label class="select-label">جنسیت</label>
+                <span class="error-message" v-if="user.gender_id_error">{{ user.gender_id_error }}</span>
+              </div>
             </div>
             <div class="input-box">
-              <v-select
-                dense
-                v-model="user.major_id"
-                label="رشته"
-                :disabled="!user.editable"
-                @input="user.hasBeenSaved = false"
-                outlined
-                :items="majors"
-                item-text="title"
-                item-value="id"
-                :rules="[user.major_id_error]"
-              />
+              <div class="select">
+                <select class="select-text" required v-model="user.major_id" :class="{ 'has-error': user.major_id_error }">
+                  <option value="" disabled selected></option>
+                  <option v-for="(item, index) in majors" :key="index" :value="item.id">{{ item.title }}</option>
+                </select>
+                <span class="select-highlight"></span>
+                <span class="select-bar"></span>
+                <label class="select-label">رشته</label>
+                <span class="error-message" v-if="user.major_id_error">{{ user.major_id_error }}</span>
+              </div>
             </div>
             <div class="input-box">
-              <v-text-field
-                dense
-                v-model="user.mobile"
-                label="موبایل"
-                :disabled="!user.editable"
-                @input="user.hasBeenSaved = false"
-                outlined
-                :rules="[user.mobile_error]"
-              />
+              <div class="form-input">
+                <label>
+                  <input :class="{ 'has-error': user.mobile_error }" required type="text" v-model="user.mobile"  @change="user.hasBeenSaved = false">
+                  <span class="placeholder">موبایل</span>
+                </label>
+                <span class="error-message" v-if="user.mobile_error">{{ user.mobile_error }}</span>
+              </div>
             </div>
             <div class="input-box">
-              <v-text-field
-                dense
-                v-model="user.nationalCode"
-                label="کد ملی"
-                :disabled="!user.editable"
-                @input="user.hasBeenSaved = false"
-                outlined
-                :rules="[user.nationalCode_error]"
-              />
+              <div class="form-input">
+                <label>
+                  <input :class="{ 'has-error': user.nationalCode_error }" required type="text" v-model="user.nationalCode"  @change="user.hasBeenSaved = false">
+                  <span class="placeholder">کد ملی</span>
+                </label>
+                <span class="error-message" v-if="user.nationalCode_error">{{ user.nationalCode_error }}</span>
+              </div>
             </div>
             <div class="input-box">
-              <v-select
-                dense
-                v-model="user.province"
-                label="استان"
-                :disabled="!user.editable"
-                @input="user.hasBeenSaved = false"
-                outlined
-                :items="provinces"
-                item-text="title"
-                item-value="id"
-                :rules="[user.province_error]"
-              />
+              <div class="select">
+                <select class="select-text" :class="{ 'has-error': user.province_error }" required v-model="user.province" @click="user.provinceDropDown = true" @blur="user.provinceDropDown =false" @change="user.provinceDropDown = false">
+                  <option value="" disabled selected></option>
+                  <option v-for="(item, index) in provincesComputed(user.provinceDropDown, user.province)" :key="index":value="item.id">{{ item.title }}</option>
+                </select>
+                <span class="select-highlight"></span>
+                <span class="select-bar"></span>
+                <label class="select-label">استان</label>
+                <span class="error-message" v-if="user.province_error">{{ user.province_error }}</span>
+              </div>
             </div>
             <div class="input-box">
-              <v-select
-                dense
-                v-model="user.shahr_id"
-                label="شهر"
-                :disabled="!user.editable"
-                @input="user.hasBeenSaved = false"
-                outlined
-                :items="selectedProvinceCity(user.province)"
-                item-text="title"
-                item-value="id"
-                :rules="[user.shahr_id_error]"
-              >
-                <template v-slot:no-data>
-                  <span :style="{ marginRight: '10px' }">ابتدا استان را انتخاب کنید</span>
-                </template>
-              </v-select>
+              <div class="select">
+                <select class="select-text" :class="{ 'has-error': user.shahr_id_error }" required v-model="user.shahr_id" @click="user.shahr_idDropdown = true" @blur="user.shahr_idDropdown = false" @change="user.shahr_idDropdown = false">
+                  <option value="" disabled selected></option>
+                  <option value="disable" disabled selected v-if="selectedProvinceCity(user.province).length === 0">ابتدا استان را انتخاب کنید</option>
+                  <option v-for="(item, index) in selectedProvinceCity(user.province, user.shahr_idDropdown, user.shahr_id)" :key="index" :value="item.id">{{ item.title }}</option>
+                </select>
+                <span class="select-highlight"></span>
+                <span class="select-bar"></span>
+                <label class="select-label">شهر</label>
+                <span class="error-message" v-if="user.shahr_id_error">{{ user.shahr_id_error }}</span>
+              </div>
             </div>
           </v-col>
           <v-col md="1" class="options">
@@ -140,19 +117,6 @@
               </template>
               <span>ذخیره شده</span>
             </v-tooltip>
-            <v-tooltip top v-else-if="isUserInfoComplete(user) && !user.loading">
-              <template v-slot:activator="{ on, attrs }">
-                <v-icon color="warning" v-bind="attrs" v-on="on">
-                  mdi-content-save
-                </v-icon>
-              </template>
-              <span>نیازمند ذخیره</span>
-            </v-tooltip>
-            <v-btn v-if="!user.editable" icon color="primary" @click="user.editable = true">
-              <v-icon>
-                mdi-pencil
-              </v-icon>
-            </v-btn>
           </v-col>
         </v-row>
       </v-form>
@@ -184,21 +148,23 @@ export default {
         this.userForm.push({
           firstName: '',
           firstNameMessage: '',
-          firstName_error: () => true,
+          firstName_error: false,
           lastName: '',
-          lastName_error: () => true,
+          lastName_error: false,
           gender_id: '',
-          gender_id_error: () => true,
+          gender_id_error: false,
           major_id: '',
-          major_id_error: () => true,
+          major_id_error: false,
           mobile: '',
-          mobile_error: () => true,
+          mobile_error: false,
           nationalCode: '',
-          nationalCode_error: () => true,
+          nationalCode_error: false,
           province: '',
-          province_error: () => true,
+          provinceDropDown: false,
+          province_error: false,
           shahr_id: '',
-          shahr_id_error: () => true,
+          shahr_idDropdown: false,
+          shahr_id_error: false,
           key: Date.now() + Math.random() * 10000,
           hasBeenSaved: false,
           editable: true,
@@ -246,7 +212,7 @@ export default {
             user.loading = false
             Object.keys(user).forEach(key => {
               if (key.includes('_error')) {
-                user[key] = () => true
+                user[key] = false
               }
             })
             setTimeout(() => {
@@ -255,64 +221,58 @@ export default {
           }).catch(err => {
             user.loading = false
             Object.keys(err.response.data.errors).forEach(key => {
-              user[key + '_error'] = () => { return err.response.data.errors[key][0] }
+              user[key + '_error'] = err.response.data.errors[key][0]
             })
             setTimeout(() => {
               this.$refs.form.validate()
             }, 500)
           })
-
-
-
-
-
-          // setTimeout(() => {
-          //   user.firstNameError = () => 'باید پر کنید'
-          //   user.key = Date.now()
-          //   setTimeout(() => {
-          //     this.$refs.form.validate()
-          //   }, 500)
-          // }, 2000)
         }
       })
     }
   },
   computed: {
-    selectedProvinceCity () {
-      return (provinceId) => {
-        if (!provinceId) {
+    provincesComputed () {
+      return (show, provinceId) => {
+        if (!show && !provinceId) {
           return []
+        } else if (!show && provinceId) {
+          return this.provinces.filter(province => province.id === provinceId)
+        }
+        return this.provinces
+      }
+    },
+    selectedProvinceCity () {
+      return (provinceId, show = true, cityId) => {
+        if (!provinceId || (!show && !cityId)) {
+          return []
+        } else if (!show && cityId) {
+          return this.cities.filter(city => city.id === cityId)
         }
         return this.cities.filter(city => city.province.id === provinceId)
       }
     },
-    savedNumber () {
-      let counter = 0
-      this.userForm.forEach(user => {
-        if (user.hasBeenSaved) {
-          counter++
-        }
-      })
-      return counter
-    },
-    completeUserNumber () {
-      let counter = 0
-      this.userForm.forEach(user => {
-        if (this.isUserInfoComplete(user)) {
-          counter++
-        }
-      })
-      return counter
-    }
   },
   created () {
-    this.initUserFormArray(true, 40)
+    this.initUserFormArray(true, 20)
     this.getUserFormData()
   }
 }
 </script>
 
 <style scoped>
+.has-been-saved {
+  background: rgba(0, 280, 0, 0.2);
+}
+
+.has-error {
+  border-color: red !important;
+}
+.error-message {
+  color: red;
+  font-size: 14px;
+}
+
 .input-box {
   padding: 0 10px;
   width: 100%;
@@ -334,3 +294,175 @@ export default {
 }
 </style>
 
+<style lang="scss">
+
+.form-input {
+  width: 100%;
+
+
+label {
+  position:relative;
+  display:block;
+  width:100%;
+  min-height:30px + 15px;
+}
+
+.placeholder {
+  position:absolute;
+  display:block;
+  color: grey;
+  top: 8px;
+  z-index:2;
+  font-size:16px;
+  transition:all 200ms ease-in-out;
+  cursor:text;
+  right: 10px;
+  background-color: #fff;
+  padding: 0 5px;
+}
+
+input {
+  position:absolute;
+  padding: 0 10px;
+  z-index:1;
+  width:100%;
+  font-size:16px;
+  border:1px solid grey;
+  border-radius: 5px;
+  transition: border-color 200ms ease-in-out;
+  outline:none;
+  margin: 0;
+}
+
+input {
+  height:40px;
+}
+
+input:focus,
+input:valid,
+textarea:focus,
+textarea:valid {
+& + .placeholder {
+    top:-10px;
+    cursor:inherit;
+    font-size: 14px;
+    color:orange;
+  }
+
+border:1px solid grey;
+}
+}
+
+
+
+
+
+.select {
+  position: relative;
+  width: 100%;
+}
+
+.select-text {
+  position: relative;
+  font-family: inherit;
+  background-color: transparent;
+  width: 100%;
+  padding: 6px 10px 5px 0;
+  font-size: 18px;
+  border-radius: 5px;
+  border: 1px solid grey;
+}
+
+/* Remove focus */
+.select-text:focus {
+  outline: none;
+  border: 1px solid grey;
+}
+
+/* Use custom arrow */
+.select .select-text {
+  appearance: none;
+  -webkit-appearance:none
+}
+
+.select:after {
+  position: absolute;
+  top: 18px;
+  left: 14px;
+  /* Styling the down arrow */
+  width: 0;
+  height: 0;
+  padding: 0;
+  content: '';
+  border-left: 5px solid transparent;
+  border-right: 5px solid transparent;
+  border-top: 5px solid grey;
+  pointer-events: none;
+}
+
+
+/* LABEL ======================================= */
+.select-label {
+  color: grey;
+  font-size: 16px;
+  font-weight: normal;
+  position: absolute;
+  pointer-events: none;
+  right: 10px;
+  top: 7px;
+  transition: 0.2s ease-in-out all;
+  background-color: #fff;
+  z-index: 2;
+  padding: 0 5px;
+}
+
+/* active state */
+.select-text:focus ~ .select-label, .select-text:valid ~ .select-label {
+  color: orange;
+  top: -10px;
+  transition: 0.2s ease all;
+  font-size: 14px;
+}
+
+/* BOTTOM BARS ================================= */
+.select-bar {
+  position: relative;
+  display: block;
+  width: 100%;
+}
+
+.select-bar:before, .select-bar:after {
+  content: '';
+  height: 3px;
+  width: 0;
+  bottom: 0px;
+  position: absolute;
+  background: orange;
+  transition: 0.2s ease all;
+}
+
+.select-bar:before {
+  left: 50%;
+}
+
+.select-bar:after {
+  right: 50%;
+}
+
+/* active state */
+.select-text:focus ~ .select-bar:before, .select-text:focus ~ .select-bar:after {
+  width: 50%;
+}
+
+/* HIGHLIGHTER ================================== */
+.select-highlight {
+  position: absolute;
+  height: 60%;
+  width: 100px;
+  top: 25%;
+  left: 0;
+  pointer-events: none;
+  opacity: 0.5;
+}
+
+</style>
